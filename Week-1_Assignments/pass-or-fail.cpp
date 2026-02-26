979a672789fa4ede08271d966a8908bce1cdf8f7
kaushtub001 <kumarakaushtub123@gmail.com>
2026-02-26 17:38:40 +0530

    done ques no 14

---
+++ Week-1_Assignments/pass-or-fail.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int M;
+  cin >> M;
+  if (M >= 35) {
+    cout << "Pass" << endl;
+  } else {
+    cout << "Fail" << endl;
+  }
+  return 0;
+}
