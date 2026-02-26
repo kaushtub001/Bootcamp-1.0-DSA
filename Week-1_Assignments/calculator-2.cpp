d2abae572ce5d798d49e604ce6ddd8300e530470
kaushtub001 <kumarakaushtub123@gmail.com>
2026-02-26 17:30:26 +0530

    done ques no 9

---
+++ Week-1_Assignments/calculator-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long N, M;
+  cin >> N >> M;
+  cout << N << " + " << M << " = " << N + M << endl << endl;
+  cout << N << " - " << M << " = " << N - M << endl << endl;
+  cout << N << " * " << M << " = " << N * M << endl << endl;
+  cout << N << " / " << M << " = " << N / M << endl << endl;
+  cout << N << " % " << M << " = " << N % M << endl << endl;
+  return 0;
+}
