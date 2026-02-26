9b918b297fb1978ddcce3e4c53215113d94b40e5
kaushtub001 <kumarakaushtub123@gmail.com>
2026-02-26 17:38:28 +0530

    done ques no 13

---
+++ Week-1_Assignments/multiple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  if (M % N == 0) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
